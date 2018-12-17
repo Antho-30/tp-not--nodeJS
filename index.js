651373b10eb002b613ef3f583b520a7e757f84b1
StephaneF <steph30470@hotmail.fr>
2018-12-17 12:02:18 +0100

    Ajout de la route /

--- index.js
+++ index.js
@@ -2,9 +2,9 @@ const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 5000 // this is very important
 
-/*app.get('/', function (req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!')
-}) */
+}) 
 
 app.listen(PORT, function () {
   console.log('Example app listening on port ' + PORT)
